5aceeea07dcd452edff493d2a8428bab57a69865
sopheak.peas <sopheak.peas@novacambodia.com>
2015-06-24 14:23:15 +0700

    Added Base theme files

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Klahan9
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'wpsp' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'wpsp' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'wpsp' ), 'wpsp', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ inc/jetpack.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ * See: https://jetpack.me/
+ *
+ * @package Klahan9
+ */
+
+/**
+ * Add theme support for Infinite Scroll.
+ * See: https://jetpack.me/support/infinite-scroll/
+ */
+function wpsp_jetpack_setup() {
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'wpsp_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+} // end function wpsp_jetpack_setup
+add_action( 'after_setup_theme', 'wpsp_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function wpsp_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		get_template_part( 'template-parts/content', get_post_format() );
+	}
+} // end function wpsp_infinite_scroll_render
